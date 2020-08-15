b9b93b77724145a446887aee090867da13925854
SeungPyo-Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2020-08-16 00:37:31 +0900

    Add files via upload

---
+++ algorithm/Chapter3/3-2.py
@@ -0,0 +1,17 @@
+'''
+5 8 3
+2 4 5 4 6
+'''
+n, m, k = map( int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort(reverse=True)
+count = 0
+
+for i in range(0,m):
+  if (i+1) % k == 0:
+    count += arr[1]
+  else:
+    count += arr[0]  
+  
+print(count);
