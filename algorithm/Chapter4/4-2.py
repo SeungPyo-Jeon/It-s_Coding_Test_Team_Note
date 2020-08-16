3da4dfbcbed386f6d08b712c39bcd65ffc2d8ba6
SeungPyo-Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2020-08-16 09:59:43 +0900

    Create 4-2.py

---
+++ algorithm/Chapter4/4-2.py
@@ -0,0 +1,16 @@
+'''
+Test Case 01:
+5
+Output : 11475
+'''
+
+n = int(input())
+count = 0
+
+for i in range(n+1):
+  for j in range(60):
+    for k in range(60):
+      if '3' in str(i) + str(j) + str(k):
+        count += 1
+
+print( count )
